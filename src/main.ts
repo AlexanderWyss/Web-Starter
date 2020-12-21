f2f3aed03d1f0719e1ffb4d65d9893865359a43d
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-12-21 22:53:01 +0100

    add .env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 declare const module: any;
 
+if (process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("dotenv").config();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || '3000');
